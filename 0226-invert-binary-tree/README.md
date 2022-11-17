c62b8710a46e891de415ae7bb588b01b987b828a
sparklingwaterlemon <michaelkim009@gmail.com>
2022-11-16 18:00:40 -0800

    Independent Attempt 1 for LeetCode#226 - Invert Binary Tree

---
+++ 0226-invert-binary-tree/README.md
@@ -0,0 +1,39 @@
+<h2><a href="https://leetcode.com/problems/invert-binary-tree/">
+226. Invert Binary Tree
+</a></h2>
+
+#### Easy
+
+<hr/>
+
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; _ 4 _ 
+&nbsp; &nbsp; _ 2 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 7 _
+_ 1  &nbsp; &nbsp; 3  &nbsp; &nbsp; &nbsp; &nbsp;  6 &nbsp; &nbsp; 9_
+
+<br />
+Example 1:
+```
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+```
+
+Example 2:
+```
+Input: root = [2,1,3]
+Output: [2,3,1]
+```
+
+Example 3:
+```
+Input: root = []
+Output: []
+```
+
+Constraints:
+
+- ` The number of nodes in the tree is in the range [0, 100]. `
+- ` -100 <= Node.val <= 100 `
---
+++ 0226-invert-binary-tree/attempt1 - CodePen.js
@@ -0,0 +1,55 @@
+// creating Class Constructor Nodes for Binary Tree
+class Node{
+	constructor(val, left, right){
+		this.val = (val === undefined ? 0 : val)
+		this.left = (left === undefined ? null : left)
+		this.right = (right === undefined ? null : right)
+	}
+};
+
+
+// Creating Binary Tree
+// root = [4,2,7,1,3,6,9]
+let root = new Node(4);
+let node2 = new Node(2);
+let node7 = new Node(7);
+let node1 = new Node(1);
+let node3 = new Node(3);
+let node6 = new Node(6);
+let node9 = new Node(9);
+
+root.left = node2;
+root.right = node7;
+node2.left = node1;
+node2.right = node3;
+node7.left = node6;
+node7.right = node9;
+
+
+// Invert Binary Tree Function
+var InvertTree = (tree) =>{
+	let newRoot = new Node(tree);
+		console.log(newRoot.val);
+	
+	let dummy = newRoot.val;
+	while(dummy.left){
+		var temp = dummy.left;
+		dummy.left = dummy.right;
+		dummy.right = temp;		
+		dummy = dummy.left;
+	};
+	
+	dummy = newRoot.val.right;
+	while(dummy.right){
+		var temp2 = dummy.left;
+		dummy.left = dummy.right;
+		dummy.right = temp2;
+		dummy = dummy.right;
+	};
+	
+	console.log(newRoot.val)
+}
+
+InvertTree(root);
+
+// This solution does not "run" in LeetCode :-(
